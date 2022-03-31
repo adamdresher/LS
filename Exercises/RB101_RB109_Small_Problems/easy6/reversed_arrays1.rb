a81b28b83dd9417919ee5533998ab4c2610a3609
Adam Dresher <adamdresher@gmail.com>
2022-03-31 17:18:05 +0700

    add exercise file

---
+++ Exercises/RB101_RB109_Small_Problems/easy6/reversed_arrays1.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# def reverse!(list)
+#   reversed_list = []
+
+#   (reversed_list << list.pop)   until list.empty?
+#   (list << reversed_list.shift) until reversed_list.empty?
+
+#   list
+# end
+
+# Solution 2.
+# frozen_string_literal: true
+
+def reverse!(list)
+  list.map!.with_index { |ele, i| [ele, list.size - i] }
+      .sort! { |a, b| a[1] <=> b[1] }
+      .map! { |ele| ele[0] }
+  list
+end
