6aa92bf9e07f727d6fa0d15fe7598563c1c8c7bf
Adam Dresher <adamdresher@gmail.com>
2022-04-06 22:35:35 +0700

    add exercise files

---
+++ Exercises/RB101_RB109_Small_Problems/easy7/04_swapcase.rb
@@ -0,0 +1,29 @@
+# def swapcase(string)
+#   new_string = ''
+
+#   string.each_char do |char|
+#     new_string << (char == char.upcase ? char.downcase : char.upcase)
+#   end
+
+#   new_string
+# end
+
+# Solution 2.
+# frozen_string_literal: true
+
+ALPHABET = [('a'..'z').to_a, ('A'..'Z').to_a]
+
+def swapcase(string)
+  string.chars.map do |char|
+      if ALPHABET[0].include?(char)
+        ALPHABET[1][ALPHABET[0].find_index(char)]
+      elsif ALPHABET[1].include?(char)
+        ALPHABET[0][ALPHABET[1].find_index(char)]
+      else
+        char
+      end
+  end.join
+end
+
+p swapcase('CamelCase') == 'cAMELcASE'
+p swapcase('Tonight on XYZ-TV') == 'tONIGHT ON xyz-tv'
