e82419073ad6cb5d886f359a617254c44c3cd34f
Adam Dresher <adamdresher@gmail.com>
2022-04-20 18:28:21 +0700

    update project file

--- RB101/lesson6/tictactoe/tictactoe.rb
+++ RB101/lesson6/tictactoe/tictactoe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -7,6 +9,7 @@ def prompt(msg)
 end
 
 def display_board(brd)
+  system 'clear'
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -26,20 +29,39 @@ def initialize_board
   (1..9).each_with_object({}) {|num, hsh| hsh[num] = INITIAL_MARKER}
 end
 
-def empty_spaces(brd)
+def empty_squares(brd)
   brd.keys.select {|key| brd[key] == INITIAL_MARKER}
 end
 
 def player_places_piece!(brd)
   loop do
-    prompt("Choose a square #{empty_spaces(brd)}:")
+    prompt("Choose a square #{empty_squares(brd)}:")
     square = gets.chomp.to_i
-    return brd[square] = PLAYER_MARKER if empty_spaces(brd).include? square
+    return brd[square] = PLAYER_MARKER if empty_squares(brd).include? square
 
     prompt("Sorry, #{square} is an invalid square.")
   end
 end
 
-display_board(board)
-player_places_piece!(board)
-display_board(board)
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  false
+end
+
+board = initialize_board
+
+loop do
+  display_board(board)
+  player_places_piece!(board)
+  computer_places_piece!(board)
+  display_board(board)
+  break if someone_won?(board) || board_full?(board)
+end
