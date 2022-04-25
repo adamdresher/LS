805d5dff27b0ae042af1bf5c761864a80b6c3710
Adam Dresher <adamdresher@gmail.com>
2022-04-25 16:33:32 +0700

    update program: gameplay loop, determining points/winner, player turn

--- RB101/lesson6/twenty_one/twenty_one.rb
+++ RB101/lesson6/twenty_one/twenty_one.rb
@@ -1,3 +1,6 @@
+require 'pry'
+require 'pry-byebug'
+
 CARD_NAMES = %w(2 3 4 5 6 7 8 9 10 jack queen king ace)
 WORTH = {'2'=>2, '3'=>3, '4'=>4, '5'=>5, '6'=>6, '7'=>7, '8'=>8, '9'=>9, '10'=>10, 'jack'=>10, 'queen'=>10, 'king'=>10, 'ace'=>[1, 11]}
 
@@ -9,22 +12,26 @@ def initialize_deck
   CARD_NAMES * 4
 end
 
-def initial_hand(dck)
+def draw_card(dck)
+  dck.pop
+end
+
+def initialize_hand(dck)
   player_hand, dealer_hand = [], []
 
   2.times { dck.shuffle! }
   2.times do
-    player_hand << dck.pop
-    dealer_hand << dck.pop
+    player_hand << draw_card(dck)
+    dealer_hand << draw_card(dck)
   end
 
   [player_hand, dealer_hand]
 end
 
-def display_board(playr_hnd, dealr_hnd, reveal_dealrs_crd)
+def display_board(playr_hnd, dealr_hnd, reveal_dealr_crd=false)
   prompt "You have: #{joinor(playr_hnd)}"
 
-  if reveal_dealrs_crd
+  if reveal_dealr_crd
     prompt "Dealer has: #{joinor(dealr_hnd)}"
   else
     prompt "Dealer has: #{dealr_hnd[0]} and unknown card."
@@ -38,48 +45,64 @@ def joinor(hnd) # not accounting for dealer's hidden card
   end
 end
 
-def player_turn(dck, playr_hnd, bust, reveal_dealrs_crd)
-  reveal_dealrs_crd = true
-  if 
-end
-
-def dealer_turn(dck, dealr_hnd, bust)
-end
-
-def determine_points_winner(playr_hnd, dealr_hnd) # does not handle aces
-  player = playr_hnd.map { |card| WORTH[card] }.sum
-  dealer = dealr_hnd.map { |card| WORTH[card] }.sum
-  [player, dealer]
-end
-
-def someone_busts(dtrmn_pnts_wnnr, playr_hnd, dealr_hnd)
-  'Dealer' if dtrmn_pnts_wnnr(playr_hnd, dealr_hnd)[1] > 21
-  'Player' if dtrmn_pnts_wnnr(playr_hnd, dealr_hnd)[0] > 21
-end
-
-def display_winner(bust, dtrmn_pnts_wnnr, playr_hnd, dealr_hnd)
-  case bust
-  when 'Player' then 'Dealer'
-  when 'Dealer' then 'Player'
-  else dtrmn_pnts_wnnr(playr_hnd, dealr_hnd)
+def player_turn!(dck, playr_hnd, dealr_hnd)
+  loop do
+    break if someone_busts(playr_hnd)
+    prompt "Would you like to draw or stay?"
+    choice = gets[0].downcase
+    if choice == 'd'
+      playr_hnd.push(draw_card(dck))
+    elsif choice == 's'
+      break
+    end
+    display_board(playr_hnd, dealr_hnd)
   end
+
+end
+
+def dealer_turn!(dck, dealr_hnd)
+end
+
+def determine_points(hnd) # does not handle aces
+  hnd.map { |card| WORTH[card] }.sum
+end
+
+def determine_points_winner(playr_hnd, dealr_hnd)
+  playr = determine_points(playr_hnd)
+  dealr = determine_points(dealr_hnd)
+  playr > dealr ? 'Player wins!' : 'Dealer wins!'
+end
+
+def someone_busts(hnd)
+  true if determine_points(hnd) > 21
+end
+
+def display_winner(playr_hnd, dealr_hnd)
+  winner = case
+           when someone_busts(playr_hnd) then 'Dealer wins!'
+           when someone_busts(dealr_hnd) then 'Player wins!'
+           else determine_points_winner(playr_hnd, dealr_hnd)
+           end
+  display_board(playr_hnd, dealr_hnd, true)
+  prompt(winner)
 end
 
 # gameplay, start
 loop do
   deck = CARD_NAMES * 4
-  player_hand, dealer_hand = initial_hand(deck)
-  reveal_dealers_card = false
+  player_hand, dealer_hand = initialize_hand(deck)
 
-  display_board(player_hand, dealer_hand, reveal_dealers_card)
-  # player_turn(deck, player_hand, reveal_dealers_card)
-  break if someone_busts(dtrmn_pnts_wnnr, playr_hnd, dealr_hnd)
-  # dealer_turn(deck, dealer_hand)
-  break if someone_busts(dtrmn_pnts_wnnr, playr_hnd, dealr_hnd)
-  display_winner(someone_busts, determine_points_winner, playr_hnd, dealr_hnd)
+  # binding.pry
+  display_board(player_hand, dealer_hand)
+  player_turn!(deck, player_hand, dealer_hand)
+  return display_winner(player_hand, dealer_hand) if someone_busts(player_hand)
+
+  dealer_turn!(deck, dealer_hand)
+  return display_winner(player_hand, dealer_hand) if someone_busts(dealer_hand)
+
+  display_winner(player_hand, dealer_hand)
+  break
 end
-
-display_winner(someone_busts, determine_points_winner, playr_hnd, dealr_hnd)
 
 # Algorithm:
 # 1. Initialize a deck:
