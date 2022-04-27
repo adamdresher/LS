fad4bdace903972a634740ffbb3e3cda208f53eb
Adam Dresher <adamdresher@gmail.com>
2022-04-27 21:36:42 +0700

    update program: appeased rubocop

--- RB101/lesson6/twenty_one/twenty_one.rb
+++ RB101/lesson6/twenty_one/twenty_one.rb
@@ -1,7 +1,9 @@
 CARD_NAMES = %w(2 3 4 5 6 7 8 9 10 jack queen king ace)
-WORTH = {'2'=>[2], '3'=>[3], '4'=>[4], '5'=>[5], '6'=>[6], '7'=>[7], '8'=>[8], 
-'9'=>[9], '10'=>[10], 'jack'=>[10], 'queen'=>[10], 'king'=>[10], 'ace'=>[1, 11]}
-PLAYER_WINS = ['Dealer busts, ', 'Player wins!']
+# suit isn't relevant to the game requirements, so it was ignored
+WORTH = { '2' => [2], '3' => [3], '4' => [4], '5' => [5], '6' => [6],
+          '7' => [7], '8' => [8], '9' => [9], '10' => [10], 'jack' => [10],
+          'queen' => [10], 'king' => [10], 'ace' => [1, 11] }
+PLAYER_WINS = ['Dealer busts, ', 'Player wins!'] # should use a YAML
 DEALER_WINS = ['Player busts, ', 'Dealer wins!']
 
 def prompt(msg)
@@ -29,7 +31,8 @@ def draw_card!(dck)
 end
 
 def initialize_hand(dck)
-  player_hand, dealer_hand = [], []
+  player_hand = []
+  dealer_hand = []
 
   2.times { dck.shuffle! }
   2.times do
@@ -38,6 +41,22 @@ def initialize_hand(dck)
   end
 
   [player_hand, dealer_hand]
+end
+
+def display_greeting
+  clear_screen
+  new_line(6) # can create a YAML if there's time
+  puts "                          Welcome to Twenty-One!"
+  pause(1)
+  new_line
+  puts "                                GAME RULES:"
+  puts "    The goal of the game is to reach as close to 21 without going over."
+  puts "                         (The dealer wins a tie =p)"
+  new_line
+  puts "    You can (d)raw a card or (s)tay and let the dealer begin its turn."
+  new_line(2)
+  puts "                 Press Enter when you are ready to begin."
+  gets.chomp
 end
 
 def display_board(playr_hnd, dealr_hnd, reveal_dealr_crd=false)
@@ -54,16 +73,20 @@ def display_board(playr_hnd, dealr_hnd, reveal_dealr_crd=false)
 end
 
 def joinor(hnd)
-  case hnd.size
-  when 2 then hnd.join(' & ')
-  when 3.. then hnd[0..-2].join(', ') << ', & ' << hnd[-1].to_s
+  if hnd.size == 2
+    hnd.join(' & ')
+  elsif hnd.size >= 3
+    hnd[0..-2].join(', ') + ', & ' + hnd[-1].to_s
   end
 end
 
+# rubocop: disable Metrics/MethodLength, Metrics/AbcSize
+# offenses are due to string outputs
 def player_turn!(dck, playr_hnd, dealr_hnd)
   loop do
     break if someone_busts(playr_hnd)
     choice = nil
+
     loop do
       clear_screen
       display_board(playr_hnd, dealr_hnd)
@@ -75,13 +98,12 @@ def player_turn!(dck, playr_hnd, dealr_hnd)
       prompt "Sorry, please enter 'd' for draw or 's' for stay."
     end
 
-    case choice
-    when 'd'
+    if choice == 'd'
       new_card = draw_card!(dck)
       prompt "Player receives a #{new_card}."
       playr_hnd << new_card
       pause(0.5)
-    when 's'
+    elsif choice == 's'
       pause(0.5)
       break
     end
@@ -109,11 +131,13 @@ def dealer_turn!(dck, playr_hnd, dealr_hnd)
       break
     end
     pause(1)
-  end  
+  end
 end
+# rubocop: enable Metrics/MethodLength, Metrics/AbcSize
 
+# aces are calculated last to ensure aces can be reassigned
 def aces_go_last(hnd)
-  hnd.each_with_object([]) do |card,new_hnd|
+  hnd.each_with_object([]) do |card, new_hnd|
     card == 'ace' ? new_hnd.append(card) : new_hnd.prepend(card)
   end
 end
@@ -123,11 +147,11 @@ def determine_points(hnd)
   hnd = aces_go_last(hnd)
 
   hnd.flat_map do |card|
-    if card == 'ace'
-      hand_val << (hand_val.flatten.sum > 10 ? WORTH[card][0] : WORTH[card][1])
-    else
-      hand_val << WORTH[card][0]
-    end
+    hand_val << if card == 'ace'
+                  (hand_val.flatten.sum > 10 ? WORTH[card][0] : WORTH[card][1])
+                else
+                  WORTH[card][0]
+                end
   end
   hand_val.sum
 end
@@ -139,14 +163,16 @@ def determine_points_winner(playr_hnd, dealr_hnd)
 end
 
 def someone_busts(hnd)
-  true if determine_points(hnd) > 21
+  determine_points(hnd) > 21
 end
 
 def display_winner(playr_hnd, dealr_hnd)
-  winner = case
-           when someone_busts(playr_hnd) then DEALER_WINS.join
-           when someone_busts(dealr_hnd) then PLAYER_WINS.join
-           else determine_points_winner(playr_hnd, dealr_hnd)
+  winner = if someone_busts(playr_hnd)
+             DEALER_WINS.join
+           elsif someone_busts(dealr_hnd)
+             PLAYER_WINS.join
+           else
+             determine_points_winner(playr_hnd, dealr_hnd)
            end
   display_board(playr_hnd, dealr_hnd, true)
   new_line
@@ -154,19 +180,7 @@ def display_winner(playr_hnd, dealr_hnd)
 end
 
 # main loop
-clear_screen
-new_line(6)
-puts "                             Welcome to Twenty-One!"
-pause(1)
-new_line
-puts "                                   GAME RULES:"
-puts "       The goal of the game is to reach as close to 21 without going over.
-                            (The dealer wins a tie =p)"
-new_line
-puts "       You can (d)raw a card or (s)tay and let the dealer begin its turn."
-new_line(2)
-puts "                    Press Enter when you are ready to begin."
-gets.chomp
+display_greeting
 
 loop do
   deck = CARD_NAMES * 4
