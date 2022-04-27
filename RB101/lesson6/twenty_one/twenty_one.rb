befe6a466672bf4fe98fe7093097d494805595e1
Adam Dresher <adamdresher@gmail.com>
2022-04-27 16:38:12 +0700

    update program: account for aces

--- RB101/lesson6/twenty_one/twenty_one.rb
+++ RB101/lesson6/twenty_one/twenty_one.rb
@@ -2,7 +2,7 @@ require 'pry'
 require 'pry-byebug'
 
 CARD_NAMES = %w(2 3 4 5 6 7 8 9 10 jack queen king ace)
-WORTH = {'2'=>2, '3'=>3, '4'=>4, '5'=>5, '6'=>6, '7'=>7, '8'=>8, '9'=>9, '10'=>10, 'jack'=>10, 'queen'=>10, 'king'=>10, 'ace'=>[1, 11]}
+WORTH = {'2'=>[2], '3'=>[3], '4'=>[4], '5'=>[5], '6'=>[6], '7'=>[7], '8'=>[8], '9'=>[9], '10'=>[10], 'jack'=>[10], 'queen'=>[10], 'king'=>[10], 'ace'=>[1, 11]}
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -29,16 +29,16 @@ def initialize_hand(dck)
 end
 
 def display_board(playr_hnd, dealr_hnd, reveal_dealr_crd=false)
-  prompt "You have: #{joinor(playr_hnd)}"
+  prompt "You have: #{joinor(playr_hnd)} (#{determine_points(playr_hnd)})"
 
   if reveal_dealr_crd
-    prompt "Dealer has: #{joinor(dealr_hnd)}"
+    prompt "Dealer has: #{joinor(dealr_hnd)} (#{determine_points(dealr_hnd)})"
   else
-    prompt "Dealer has: #{dealr_hnd[0]} and unknown card."
+    prompt "Dealer has: #{dealr_hnd[0]} and unknown card (#{determine_points(dealr_hnd)})"
   end
 end
 
-def joinor(hnd) # not accounting for dealer's hidden card
+def joinor(hnd)
   case hnd.size
   when 2 then hnd.join(' & ')
   when 3.. then hnd[0..-2].join(', ') << ', & ' << hnd[-1].to_s
@@ -50,21 +50,38 @@ def player_turn!(dck, playr_hnd, dealr_hnd)
     break if someone_busts(playr_hnd)
     prompt "Would you like to draw or stay?"
     choice = gets[0].downcase
+
     if choice == 'd'
       playr_hnd.push(draw_card(dck))
     elsif choice == 's'
       break
     end
-    display_board(playr_hnd, dealr_hnd)
-  end
 
+    display_board(playr_hnd, dealr_hnd, true) # set true for debugging
+  end
 end
 
 def dealer_turn!(dck, dealr_hnd)
 end
 
-def determine_points(hnd) # does not handle aces
-  hnd.map { |card| WORTH[card] }.sum
+def aces_go_last(hnd)
+  hnd.each_with_object([]) do |card,new_hnd|
+    card == 'ace' ? new_hnd.append(card) : new_hnd.prepend(card)
+  end
+end
+
+def determine_points(hnd)
+  hand_val = []
+  hnd = aces_go_last(hnd)
+
+  hnd.flat_map do |card|
+    if card == 'ace'
+      hand_val << (hand_val.flatten.sum > 10 ? WORTH[card][0] : WORTH[card][1])
+    else
+      hand_val << WORTH[card][0]
+    end
+  end
+  hand_val.sum
 end
 
 def determine_points_winner(playr_hnd, dealr_hnd)
@@ -92,7 +109,7 @@ loop do
   deck = CARD_NAMES * 4
   player_hand, dealer_hand = initialize_hand(deck)
   # binding.pry
-  display_board(player_hand, dealer_hand)
+  display_board(player_hand, dealer_hand, true) # set true for debugging
   player_turn!(deck, player_hand, dealer_hand)
   return display_winner(player_hand, dealer_hand) if someone_busts(player_hand)
 
@@ -121,3 +138,10 @@ end
 # 8. Play again? 
 #   - return to Step 1 if 'yes'
 #   - goodbye!
+
+
+# TODO
+# - account for aces # DONE
+# - display message when someone busts
+# - display total score when someone wins
+# - create dealer's turn
