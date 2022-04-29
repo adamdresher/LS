c844e6fb717a51b331ca3ce7f6d15bda59616ec4
Adam Dresher <adamdresher@gmail.com>
2022-04-29 16:07:45 +0700

    suit implementation surgury done, update yml format, appease rubocop

--- RB101/lesson6/twenty_one/twenty_one.rb
+++ RB101/lesson6/twenty_one/twenty_one.rb
@@ -1,12 +1,10 @@
 require 'yaml'
 
 MESSAGES = YAML.load_file('twenty_one_messages.yml')
-# SUITS = %w(spade heart club diamond)
 
-SUITS = [[9828].pack('U*'), [9825].pack('U*'), [9831].pack('U*'), [9826].pack('U*')]
-
+SUITS = [[9828].pack('U*'), [9825].pack('U*'),
+         [9831].pack('U*'), [9826].pack('U*')]
 CARD_NAMES = %w(2 3 4 5 6 7 8 9 10 jack queen king ace)
-# suit isn't relevant to the game requirements, so it was ignored
 WORTH = { '2' => [2], '3' => [3], '4' => [4], '5' => [5], '6' => [6],
           '7' => [7], '8' => [8], '9' => [9], '10' => [10], 'jack' => [10],
           'queen' => [10], 'king' => [10], 'ace' => [1, 11] }
@@ -21,8 +19,8 @@ def prompt_yaml(msg)
   puts "=> #{MESSAGES[msg]}"
 end
 
-def prompt_yaml_(msg)
-  puts MESSAGES[msg]
+def prompt_yaml_centered(msg)
+  puts MESSAGES[msg].center(80)
 end
 
 def new_line(lines=1)
@@ -68,8 +66,8 @@ def initialize_hand!(mtch_ttl, dck)
     hands[:dealr] << draw_card!(dck)
   end
 
-  # update_points!(hands[:playr], mtch_ttl, :playr_points)
-  # update_points!(hands[:dealr], mtch_ttl, :dealr_points)
+  update_points!(hands[:playr], mtch_ttl, :playr_points)
+  update_points!(hands[:dealr], mtch_ttl, :dealr_points)
 
   hands
 end
@@ -79,59 +77,59 @@ def draw_card!(dck)
 end
 
 def create_cards(cards)
-  cards.map {|card| "#{card[0]} of #{card[1]}"}
+  cards.map { |card| "#{card[0]} of #{card[1]}" }
+end
+
+def create_first_card(card)
+  "#{card[0]} of #{card[1]}"
 end
 
 def display_greeting
   clear_screen
   new_line 6
-  prompt_yaml_ 'welcome'
+  prompt_yaml_centered 'welcome'
   new_line
-  puts SUITS
-  prompt_yaml_ 'rules0'
-  prompt_yaml_ 'rules1' # should figure out how to add variables to yaml
-  puts "   Win a hand/match by getting as close to #{SCORE_LIMIT}\
- as possible without going over."
-  prompt_yaml_ 'rules3'
+  print SUITS.join('  ').center(80)
+  new_line(2)
+end
+
+def display_rules
+  prompt_yaml_centered 'rules0'
   new_line
-  prompt_yaml_ 'rules4'
+  prompt_yaml_centered 'rules1' # should figure out how to add variables to yaml
+  puts "  Win a hand/match by getting as close to #{SCORE_LIMIT} \
+as possible without going over."
+  prompt_yaml_centered 'rules3'
+  new_line
+  prompt_yaml_centered 'rules4'
   new_line 2
-  prompt_yaml_ 'rules5'
+  prompt_yaml_centered 'rules5'
   gets
 end
 
-# delr_hnd is inconsistent with other methods in order to appease rubocop gods
-# def display_board(ttl_wins, mtch_ttl, hands, reveal_dealr=false)
-#   new_line
-#   puts "   Scoreboard: You - #{ttl_wins['Player']}, \
-# Dealer - #{ttl_wins['Dealer']}"
-#   prompt_yaml_ 'line'
-#   prompt "You have: #{joinor(display_cards(hands[:playr]))} \
-# ({mtch_ttl[:playr_points]} points)" # receiving array not string
-
-#   if reveal_dealr
-#     prompt "Dealer has: #{joinor(display_cards(hands[:dealr]))} \
-# ({mtch_ttl[:dealr_points]} points)"
-#   else
-#     prompt "Dealer has: #{display_cards(hands[:dealr][0])} and an unknown card"
-#   end
-# end
-
-# testing out display
-def test_display_board(hands, reveal_dealr=false)
+def display_scoreboard(ttl_wins)
   new_line
-#  puts "   Scoreboard: You - #{ttl_wins['Player']}, \
-#Dealer - #{ttl_wins['Dealer']}"
-  prompt_yaml_ 'line'
-  prompt "You have: #{joinor(create_cards(hands[:playr]))}"# \
-#({mtch_ttl[:playr_points]} points)" # receiving array not string
+  puts "   \
+Scoreboard: You - #{ttl_wins['Player']}, Dealer - #{ttl_wins['Dealer']}"
+  puts MESSAGES['line']
+end
 
-  # if reveal_dealr
-    prompt "Dealer has: #{joinor(create_cards(hands[:dealr]))}"# \
-#({mtch_ttl[:dealr_points]} points)"
-  # else
-    prompt "Dealer has: #{create_cards(hands[:dealr][0])} and an unknown card"
-  # end
+def display_hands(mtch_ttl, hands, reveal_dealr=false)
+  prompt "You have: \
+#{joinor(create_cards(hands[:playr]))} (#{mtch_ttl[:playr_points]} points)"
+
+  if reveal_dealr
+    prompt "Dealer has: \
+#{joinor(create_cards(hands[:dealr]))} (#{mtch_ttl[:dealr_points]} points)"
+  else
+    prompt "Dealer has: \
+#{create_first_card(hands[:dealr].first)} and an unknown card"
+  end
+end
+
+def display_board(ttl_wins, mtch_ttl, hands, reveal_dealr=false)
+  display_scoreboard(ttl_wins)
+  display_hands(mtch_ttl, hands, reveal_dealr)
 end
 
 def joinor(hnd)
@@ -161,7 +159,7 @@ end
 
 def player_draws!(mtch_ttl, dck, playr_hnd)
   new_card = draw_card!(dck)
-  prompt "Player receives a #{new_card}."
+  prompt "Player receives a #{create_first_card(new_card)}."
   playr_hnd << new_card
 
   update_points!(playr_hnd, mtch_ttl, :playr_points)
@@ -189,7 +187,7 @@ def dealer_draws!(dck, mtch_ttl, dealr_hnd)
   pause 1
   new_line
   new_card = draw_card!(dck)
-  prompt "Dealer receives a #{new_card}."
+  prompt "Dealer receives a #{create_first_card(new_card)}."
   dealr_hnd << new_card
 
   update_points!(dealr_hnd, mtch_ttl, :dealr_points)
@@ -211,7 +209,6 @@ def dealer_turn!(ttl_wins, mtch_ttl, dck, hands)
     else
       prompt_yaml 'dealer_stay'
       break
-
     end
     pause 1
   end
@@ -220,7 +217,7 @@ end
 # aces are calculated last to ensure their values can be properly
 def aces_go_last(hnd)
   hnd.each_with_object([]) do |card, new_hnd|
-    card == 'ace' ? new_hnd.append(card) : new_hnd.prepend(card)
+    card[0] == 'ace' ? new_hnd.append(card) : new_hnd.prepend(card)
   end
 end
 
@@ -229,14 +226,14 @@ def update_points!(hnd, mtch_ttl, current_turn)
   hnd = aces_go_last(hnd)
 
   hnd.flat_map do |card|
-    hand_val << if card == 'ace'
-                  if hand_val.flatten.sum > (SCORE_LIMIT - 11)
-                    WORTH[card][0]
+    hand_val << if card[0] == 'ace'
+                  if hand_val.sum > (SCORE_LIMIT - 11)
+                    WORTH[card[0]][0]
                   else
-                    WORTH[card][1]
+                    WORTH[card[0]][1]
                   end
                 else
-                  WORTH[card][0]
+                  WORTH[card[0]][0]
                 end
   end
   mtch_ttl[current_turn] = hand_val.sum
@@ -287,62 +284,41 @@ def found_game_winner?(ttl_wins)
 end
 
 # gameplay starts
-# display_greeting
+display_greeting
+display_rules
 
-# loop do # main loop
-#   total_wins = Hash.new(0) # mutated with Integer#+
+loop do # main loop
+  total_wins = Hash.new(0) # mutated with Integer#+
 
-#   loop do # match loop
+  loop do # match loop
     deck = initialize_deck
-#     match_totals = Hash.new # updates when any cards are given
-#     shuffling
-    # hands = initialize_hand!(match_totals, deck)
-    hands = initialize_hand!(0, deck)
-    p hands
-    test_display_board(hands) # testing out display
-#     player_turn!(total_wins, match_totals, deck, hands)
-#     dealer_turn!(total_wins, match_totals, deck, hands)
+    match_totals = Hash.new # updates when any cards are given
+    shuffling
+    hands = initialize_hand!(match_totals, deck)
+    player_turn!(total_wins, match_totals, deck, hands)
+    dealer_turn!(total_wins, match_totals, deck, hands)
 
-#     clear_screen
+    clear_screen
 
-#     winner, busts = determine_winner(match_totals)
-#     update_match_wins!(total_wins, winner)
-#     display_board(total_wins, match_totals, hands, true)
-#     new_line
-#     display_match_winner(winner, busts)
-#     pause 1
-#     break if found_game_winner?(total_wins)
+    winner, busts = determine_winner(match_totals)
+    update_match_wins!(total_wins, winner)
+    display_board(total_wins, match_totals, hands, true)
+    new_line
+    display_match_winner(winner, busts)
+    pause 1
+    break if found_game_winner?(total_wins)
 
-#     new_line
-#     prompt_yaml 'ready_for_next_match?'
-#     gets
-#   end
+    new_line
+    prompt_yaml 'ready_for_next_match?'
+    gets
+  end
 
-#   drumroll(total_wins)
-#   pause 1
+  drumroll(total_wins)
+  pause 1
 
-#   new_line
-#   prompt_yaml 'play_again'
-#   break unless gets.chomp.downcase.start_with?('y')
-# end
+  new_line
+  prompt_yaml 'play_again'
+  break unless gets.chomp.downcase.start_with?('y')
+end
 
-# prompt_yaml 'goodbye'
-
-# NEXT
-# - add suit to cards
-
-# - update initialize_hand! to create
-# - update hands to use only card names
-# - update display to use both elements # DONE
-# - update determine_points to use only card names
-
-# | Suit | Name | unicode num |
-# | ---- | ---- | ----------- |
-# | ♠ | Black Spade | &#9824; |
-# | ♥ | Black Heart | &#9829; |
-# | ♣ | Black Club | &#9827; |
-# | ♦ | Black Diamond | &#9830; |
-# | ♤ | White Spade | &#9828; |
-# | ♡ | White Heart | &#9825; |
-# | ♧ | White Club | &#9831; |
-# | ♢ | White Diamond | &#9826; |
+prompt_yaml 'goodbye'
