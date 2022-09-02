be2dad82bc8339eb7228f6a580e637bd31a92772
Adam Dresher <adamdresher@gmail.com>
2022-09-02 08:04:59 +0700

    add exercise files

---
+++ exercises/rb120_oop/debugging/01_community_library.rb
@@ -0,0 +1,114 @@
+=begin
+class Library
+  attr_accessor :address, :phone, :books
+
+  def initialize(address, phone)
+    @address = address
+    @phone = phone
+    @books = []
+  end
+
+  def check_in(book)
+    books.push(book)
+  end
+end
+
+class Book
+  attr_accessor :title, :author, :isbn
+
+  def initialize(title, author, isbn)
+    @title = title
+    @author = author
+    @isbn = isbn
+  end
+
+  def display_data
+    puts "---------------"
+    puts "Title: #{title}"
+    puts "Author: #{author}"
+    puts "ISBN: #{isbn}"
+    puts "---------------"
+  end
+end
+
+community_library = Library.new('123 Main St.', '555-232-5652')
+learn_to_program = Book.new('Learn to Program', 'Chris Pine', '978-1934356364')
+little_women = Book.new('Little Women', 'Louisa May Alcott', '978-1420951080')
+wrinkle_in_time = Book.new('A Wrinkle in Time', 'Madeleine L\'Engle', '978-0312367541')
+
+community_library.check_in(learn_to_program)
+community_library.check_in(little_women)
+community_library.check_in(wrinkle_in_time)
+
+community_library.books.display_data
+=end
+
+=begin
+A NoMethodError Exception will be raised on line 43.
+Library#books returns an Array of Book objects, while Book#display_data requires
+a Book object.
+
+There are two approaches to resolving this issue.  The simplest approach would
+to chain #each to #books and pass #display_data to each Book object.  This
+solution seems appropriate if this behavior is only required once.
+If this behavior would be used repetitively, a better approach would be to
+adjust the objects.  This could be done by creating a #display_books method
+available for Library which displayed the display_data of each Book.  Taking
+this one step further, each Book could return the display data with #to_s.  This
+would eliminate the need for the method Book#display_data, replacing it with a
+simple String interpolation behavior.
+
+Given that libraries often make their catalogue of books available, it seems
+reasonable to assume this #display_data behavior would be used frequently.
+=end
+
+# approach one
+# community_library.books.each(&:display_data)
+
+# approach two
+class Library
+  attr_accessor :address, :phone, :books
+
+  def initialize(address, phone)
+    @address = address
+    @phone = phone
+    @books = []
+  end
+
+  def check_in(book)
+    books.push(book)
+  end
+
+  def display_books
+    books.each { |book| puts book }
+  end
+end
+
+class Book
+  attr_accessor :title, :author, :isbn
+
+  def initialize(title, author, isbn)
+    @title = title
+    @author = author
+    @isbn = isbn
+  end
+
+  def to_s
+    puts "---------------"
+    puts "Title: #{title}"
+    puts "Author: #{author}"
+    puts "ISBN: #{isbn}"
+    puts "---------------"
+  end
+end
+
+community_library = Library.new('123 Main St.', '555-232-5652')
+learn_to_program = Book.new('Learn to Program', 'Chris Pine', '978-1934356364')
+little_women = Book.new('Little Women', 'Louisa May Alcott', '978-1420951080')
+wrinkle_in_time = Book.new('A Wrinkle in Time', 'Madeleine L\'Engle', '978-0312367541')
+
+community_library.check_in(learn_to_program)
+community_library.check_in(little_women)
+community_library.check_in(wrinkle_in_time)
+
+community_library.display_books
---
+++ exercises/rb120_oop/debugging/02_animal_kingdom.rb
@@ -0,0 +1,117 @@
+=begin
+class Animal
+  def initialize(diet, superpower)
+    @diet = diet
+    @superpower = superpower
+  end
+
+  def move
+    puts "I'm moving!"
+  end
+
+  def superpower
+    puts "I can #{@superpower}!"
+  end
+end
+
+class Fish < Animal
+  def move
+    puts "I'm swimming!"
+  end
+end
+
+class Bird < Animal
+end
+
+class FlightlessBird < Bird
+  def initialize(diet, superpower)
+    super
+  end
+
+  def move
+    puts "I'm running!"
+  end
+end
+
+class SongBird < Bird
+  def initialize(diet, superpower, song)
+    super
+    @song = song
+  end
+
+  def move
+    puts "I'm flying!"
+  end
+end
+
+# Examples
+
+unicornfish = Fish.new(:herbivore, 'breathe underwater')
+penguin = FlightlessBird.new(:carnivore, 'drink sea water')
+robin = SongBird.new(:omnivore, 'sing', 'chirp chirrr chirp chirp chirrrr')
+=end
+
+=begin
+Line 51 will raise an `ArgumentError` `Exception` due to the implementation of
+`SongBird`'s implementation of `initialize`.  The keyword `super` is called
+without defining any arguments, so `super` uses a default implementation and
+accepts all `initialize`'s' arguments- 3 arguments to be specific.  `super` will
+search for the next definition of `initialize` up `SongBird`'s ancestors, which
+is `Animal`.  Howeever `Animal`'s implementation of `initialize` only accepts 2
+arguments.  So an `ArgumentError` will be raised along with some additional
+information regarding the number of arguments given/expected being displayed.
+
+This could be resolved by defining the two arguments that should be passed to
+`super` on line 38.
+=end
+
+class Animal
+  def initialize(diet, superpower)
+    @diet = diet
+    @superpower = superpower
+  end
+
+  def move
+    puts "I'm moving!"
+  end
+
+  def superpower
+    puts "I can #{@superpower}!"
+  end
+end
+
+class Fish < Animal
+  def move
+    puts "I'm swimming!"
+  end
+end
+
+class Bird < Animal
+end
+
+class FlightlessBird < Bird
+  def initialize(diet, superpower)
+    super
+  end
+
+  def move
+    puts "I'm running!"
+  end
+end
+
+class SongBird < Bird
+  def initialize(diet, superpower, song)
+    super(diet, superpower)
+    @song = song
+  end
+
+  def move
+    puts "I'm flying!"
+  end
+end
+
+# Examples
+
+unicornfish = Fish.new(:herbivore, 'breathe underwater')
+penguin = FlightlessBird.new(:carnivore, 'drink sea water')
+robin = SongBird.new(:omnivore, 'sing', 'chirp chirrr chirp chirp chirrrr')
