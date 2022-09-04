2e81d42920572d077d0fd4c087515b15da5f30f1
Adam Dresher <adamdresher@gmail.com>
2022-09-04 19:58:47 +0700

    update for edge cases

--- exercises/rb120_oop/debugging/10_does_it_rock_or_not.rb
+++ exercises/rb120_oop/debugging/10_does_it_rock_or_not.rb
@@ -81,18 +81,92 @@ the `AuthenticationError` `Exception` to be returned to `Score#for_term` and
 thus being rescued on line 51 within `Score#find_out`'s `case` block.  Using
 this approach negates the need for the condition on lines 42-43 and thus the
 `NoScore` class being defined at all, on line 25.
+However, this approach limits the program's ability to catch `NaN` errors.  A
+`NaN` error will occur when both `positive` and `negative` return `0.0`,
+resulting in the expression `positive / (positive + negative` evaluating to
+`0.0 / 0.0`.  To handle this, the `NoScore` class is added back to `DoesItRock`,
+as well as returning `when NoScore; "No idea about #{term}..."` back to the
+`case` block.  The `Exception` in the guard cluse on line 33 must be replaced
+with the correct error, `ZeroDivisionError`.  Finally, `ZeroDivisionError` must
+also be raised before the rescue, in order for it to be rescued.
+
 =end
 
-module DoesItRock
+# module DoesItRock
 
-  # ...
+#   # ...
+
+#   class Score
+#     def self.for_term(term)
+#       positive = SearchEngine.count(%{"#{term} rocks"}, API_KEY).to_f
+#       negative = SearchEngine.count(%{"#{term} is not fun"}, API_KEY).to_f
+
+#       positive / (positive + negative)
+#     end
+#   end
+
+#   def self.find_out(term)
+#     score = Score.for_term(term)
+
+#     case score
+#     when 0...0.5
+#       "#{term} is not fun."
+#     when 0.5
+#       "#{term} seems to be ok..."
+#     else
+#       "#{term} rocks!"
+#     end
+#   rescue Exception => e
+#     e.message
+#   end
+# end
+
+###
+
+class AuthenticationError < Exception; end
+
+# A mock search engine
+# that returns a random number instead of an actual count.
+class SearchEngine
+  def self.count(query, api_key)
+    unless valid?(api_key)
+      raise AuthenticationError, 'API key is not valid.'
+    end
+
+    rand(200_000)
+  end
+
+  private
+
+  def self.valid?(key)
+    key == 'LS1A'
+  end
+end
+
+module DoesItRock
+  API_KEY = 'LS1A'
+
+  class NoScore; end
 
   class Score
     def self.for_term(term)
       positive = SearchEngine.count(%{"#{term} rocks"}, API_KEY).to_f
       negative = SearchEngine.count(%{"#{term} is not fun"}, API_KEY).to_f
 
-      positive / (positive + negative)
+      score = positive / (positive + negative)
+
+      raise ZeroDivisionError unless score.finite?
+
+      score
+
+      rescue ZeroDivisionError
+        NoScore.new
+    end
+
+    private
+
+    def self.valid?(score)
+      !score.nan?
     end
   end
 
@@ -100,6 +174,8 @@ module DoesItRock
     score = Score.for_term(term)
 
     case score
+    when NoScore
+      "No idea about #{term}..."
     when 0...0.5
       "#{term} is not fun."
     when 0.5
@@ -107,7 +183,14 @@ module DoesItRock
     else
       "#{term} rocks!"
     end
-  rescue Exception => e
+
+  rescue StandardError => e
     e.message
   end
 end
+
+# Example (your output may differ)
+
+puts DoesItRock.find_out('Sushi')       # Sushi seems to be ok...
+puts DoesItRock.find_out('Rain')        # Rain is not fun.
+puts DoesItRock.find_out('Bug hunting') # Bug hunting rocks!
