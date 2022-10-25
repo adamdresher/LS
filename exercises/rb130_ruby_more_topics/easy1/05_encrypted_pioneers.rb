69a91b77308c9ba9c2cb7c018e3b722d74b578fb
Adam Dresher <adamdresher@gmail.com>
2022-10-25 21:33:26 +0700

    add exercise file

--- exercises/rb130_ruby_more_topics/easy1/05_encrypted_pioneers.rb
+++ exercises/rb130_ruby_more_topics/easy1/05_encrypted_pioneers.rb
@@ -1,26 +1,47 @@
-def rot13(string)
-
-end
-
 # The following are encrypted names influential people in the computer science
 # field.  The encryption is Rot13, a fairly simple and weak form of encryption.
 
-p rot13("Nqn Ybirynpr")
-p rot13("Tenpr Ubccre")
-p rot13("Nqryr Tbyqfgvar")
-p rot13("Nyna Ghevat")
-p rot13("Puneyrf Onoontr")
-p rot13("Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv")
-p rot13("Wbua Ngnanfbss")
-p rot13("Ybvf Unvog")
-p rot13("Pynhqr Funaaba")
-p rot13("Fgrir Wbof")
-p rot13("Ovyy Tngrf")
-p rot13("Gvz Orearef-Yrr")
-p rot13("Fgrir Jbmavnx")
-p rot13("Xbaenq Mhfr")
-p rot13("Fve Nagbal Ubner")
-p rot13("Zneiva Zvafxl")
-p rot13("Lhxvuveb Zngfhzbgb")
-p rot13("Unllvz Fybavzfxv")
-p rot13("Tregehqr Oynapu")
+PIONEERS = ["Nqn Ybirynpr",
+            "Tenpr Ubccre",
+            "Nqryr Tbyqfgvar",
+            "Nyna Ghevat",
+            "Puneyrf Onoontr",
+            "Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv",
+            "Wbua Ngnanfbss",
+            "Ybvf Unvog",
+            "Pynhqr Funaaba",
+            "Fgrir Wbof",
+            "Ovyy Tngrf",
+            "Gvz Orearef-Yrr",
+            "Fgrir Jbmavnx",
+            "Xbaenq Mhfr",
+            "Fve Nagbal Ubner",
+            "Zneiva Zvafxl",
+            "Lhxvuveb Zngfhzbgb",
+            "Unllvz Fybavzfxv",
+            "Tregehqr Oynapu"]
+
+CHARS = ('a'..'z').to_a
+
+# de/encrypts individual characters
+def rot13_letter(char)
+  is_upcase = (char == char.upcase) ? true : false
+  char = char.downcase
+
+  if CHARS.include? char
+    char_idx = CHARS.find_index char
+    char = CHARS[char_idx - 13]
+  end
+
+  is_upcase ? char.upcase : char
+end
+
+# de/encrypts text
+def rot13(text)
+    text.split('').map { |char| rot13_letter(char) }.join
+end
+
+# decrypts and outputs list
+PIONEERS.each do |pioneer|
+  puts rot13(pioneer)
+end
